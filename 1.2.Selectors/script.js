48516a3d400bcfdf49ca7de96b9287414ed8ea99
jonathan <jonathanbajoux.pro@gmail.com>
2022-11-29 16:57:44 +0100

    jonathan

--- 1.2.Selectors/script.js
+++ 1.2.Selectors/script.js
@@ -1,2 +1,25 @@
-/*
-*/
+let jobs = document.querySelectorAll(".important")
+for (elem of jobs) {
+    elem.title = "This is an important item"
+}
+
+function cacher() {
+
+    let imageList = document.getElementsByTagName("img");
+    for (let img of imageList) {
+        if (img.className != "important") {
+            img.style.display = "none";
+        }
+    }
+}
+cacher()
+
+let paragList = document.querySelectorAll("p")
+for (let parag of paragList) {
+    if (parag.className == false) {
+        // a faire après
+    } else {
+        console.log(parag.innerText)
+    }
+    /**a changer */
+}
