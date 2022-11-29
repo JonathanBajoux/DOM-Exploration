a9f81ef04df2f0f69e6e7085fff5a8460e0d595f
jonathan <jonathanbajoux.pro@gmail.com>
2022-11-29 14:45:28 +0100

    jonathan

--- 1.1.Introduction/script.js
+++ 1.1.Introduction/script.js
@@ -6,3 +6,8 @@ document.title = 'Modifying the DOM';
 document.body.style.backgroundColor = ('#FF69B4');
 
 document.body.style.backgroundColor = "rgb(179,27,27)";
+
+let list_children = document.body.children
+for(let children of list_children) {
+  console.log(children)  
+}
