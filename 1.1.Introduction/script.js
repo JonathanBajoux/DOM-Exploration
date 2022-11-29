4822659cb4d68c1a89e37bb068020bc996b44ca6
jonathan <jonathanbajoux.pro@gmail.com>
2022-11-29 14:09:57 +0100

    jonathan

---
+++ 1.1.Introduction/script.js
@@ -0,0 +1,8 @@
+let docTitle = document.title;
+console.log(docTitle);
+
+document.title = 'Modifying the DOM';
+
+document.body.style.backgroundColor = ('#FF69B4');
+
+document.body.style.backgroundColor = "rgb(179,27,27)";
