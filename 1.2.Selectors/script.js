51804234da05fdd78d4a600cda1f92c77d12181d
jonathan <jonathanbajoux.pro@gmail.com>
2022-11-30 13:58:06 +0100

    jonathan

--- 1.2.Selectors/script.js
+++ 1.2.Selectors/script.js
@@ -3,23 +3,32 @@ for (elem of jobs) {
     elem.title = "This is an important item"
 }
 
-function cacher() {
+function main() {
 
     let imageList = document.getElementsByTagName("img");
     for (let img of imageList) {
         if (img.className != "important") {
             img.style.display = "none";
         }
+    }let parag1 = document.querySelectorAll("p")
+for (let parag2 of parag1) {
+    console.log(parag2.innerText)
+    if (parag2.className == false) {
+        parag2.style.color = getRandomColor();
+    }else{
+        console.log(parag2.className)
     }
-}
-cacher()
 
-let paragList = document.querySelectorAll("p")
-for (let parag of paragList) {
-    if (parag.className == false) {
-        // a faire après
-    } else {
-        console.log(parag.innerText)
-    }
-    /**a changer */
 }
+}
+main()
+function getRandomColor() { let r = Math.floor(Math.random() * 255); let g = Math.floor(Math.random() * 255); let b = Math.floor(Math.random() * 255); return ("rgb(" + r + "," + g + "," + b + ")") }
+
+
+
+
+
+
+
+
+
--- 1.3.Create/script.js
+++ 1.3.Create/script.js
@@ -1,2 +1,52 @@
-/*
-*/
+function addElement() {
+    let learnerArray = [
+        "Arno Volts",
+        "Aurélien Mariaule",
+        "Aurore Lemaire",
+        "Benjamin Porsont",
+        "Céline Verreydt",
+        "Corentin Miserque",
+        "Dominique Coppée",
+        "Edouard de Romrée de Vichenet",
+        "Hugo Goorickx",
+        "Jofrey Houyoux",
+        "Jonathan Manes",
+        "Jonathan Bajoux",
+        "Laura Wilhelmi",
+        "Lysie Soyez",
+        "Marnie Benalia",
+        "Mathilde Cornelis",
+        "Milo Bonnet",
+        "Nadim El Nakadi",
+        "Nathalie Vanden Abeele",
+        "Nathalie Goffette",
+        "Nour Everaert",
+        "Pierre Mauriello",
+        "Quentin Bource",
+        "Virginie Dourson"];
+        learnerArray.sort(function(){return 0.5 - Math.random()});
+    for (let learner of learnerArray) {
+        let newSection = document.createElement("section");
+        let newParag = document.createElement("p");
+        let colorIntensity = getRandomColor();
+        newSection.style.backgroundColor = colorIntensity[0];
+        newParag.innerText = learner; 
+        newSection.appendChild(newParag);
+        document.body.getElementsByTagName("article")[0].appendChild(newSection);
+        if(colorIntensity[1] <=90){
+            newParag.style.color = "white";
+        }
+    }
+
+}
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 255); 
+    let g = Math.floor(Math.random() * 255); 
+    let b = Math.floor(Math.random() * 255); 
+    return [("rgb(" + r + "," + g + "," + b + ")"),getBrightness(r,g,b)]
+}
+function getBrightness(r, g, b) {
+    return (0.2126 * r + 0.7152 * g + 0.0722 * b);
+}
+addElement()
+
